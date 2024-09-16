1ca8b86af1f375fd05d3bc5982390121c717a74e
eoe <in-eoe@outlook.com>
2024-09-16 12:41:19 +0800

    test workflows

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/smartRobot',
+  base: '/workDemo',
   build: {
     outDir: './docs'
   },
